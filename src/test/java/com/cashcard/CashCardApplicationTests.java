3a2c8a50a3d2968b128d468888f58249dbf6b8f3
yourusername <youremail@gmail.com>
2024-08-08 12:09:45 -0500

    add GET endpoint

--- src/test/java/com/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,23 @@
 package com.cashcard;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
-	}
+	void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
 
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
