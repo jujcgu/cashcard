5b5880e0ec2f55190391fa66bae5e677f7194af1
yourusername <youremail@gmail.com>
2024-08-08 10:43:06 -0500

    simple test

---
+++ src/test/java/com/cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package com.cashcard;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CashCardJsonTest {
+
+    @Test
+    void myFirstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+}
