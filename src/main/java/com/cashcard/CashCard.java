7d7b356a9befff5ac249d01150803bb8544d5c8f
jujcgu <juanjosecastrogitierrez@@gmail.com>
2024-08-18 18:47:26 -0500

    add repositories and spring data

--- src/main/java/com/cashcard/CashCard.java
+++ src/main/java/com/cashcard/CashCard.java
@@ -1,5 +1,6 @@
 package com.cashcard;
 
+// Add this import
 import org.springframework.data.annotation.Id;
 
 record CashCard(@Id Long id, Double amount) {
--- src/main/java/com/cashcard/CashCardController.java
+++ src/main/java/com/cashcard/CashCardController.java
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
-    private final CashCardRepository cashCardRepository;
+   private final CashCardRepository cashCardRepository;
 
-    private CashCardController(CashCardRepository cashCardRepository) {
-        this.cashCardRepository = cashCardRepository;
-    }
+   private CashCardController(CashCardRepository cashCardRepository) {
+      this.cashCardRepository = cashCardRepository;
+   }
 
-    @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
-        if (cashCardOptional.isPresent()) {
-            return ResponseEntity.ok(cashCardOptional.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+   @GetMapping("/{requestedId}")
+   private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+       Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+       if (cashCardOptional.isPresent()) {
+           return ResponseEntity.ok(cashCardOptional.get());
+       } else {
+           return ResponseEntity.notFound().build();
+       }
+   }
 }
